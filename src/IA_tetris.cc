3af959dc8fa4f455ac5661eaae14846aadaa8c32
SCIMONE LUCA <luca.scimone@etu.unistra.fr>
2021-04-11 17:46:34 +0200

    debut ia

--- src/IA_tetris.cc
+++ src/IA_tetris.cc
@@ -1,16 +1,16 @@
 #include "board.hpp"
 
-board = Board()
-def IA_play()
-{
-    Piece cur_p = board->getcurPiece();
-    posx = cur_p.getX;
-    posy = cur_p.getY;
-    orientation = cur_p.
-    if (board->isPieceMovable(x+1,y))
-    {
-        if(board->isPieceRotable(o))
-    }
+// board = Board()
+// def IA_play()
+// {
+//     Piece cur_p = board->getcurPiece();
+//     posx = cur_p.getX;
+//     posy = cur_p.getY;
+//     orientation = cur_p.
+//     if (board->isPieceMovable(x+1,y))
+//     {
+//         if(board->isPieceRotable(o))
+//     }
 
 
-}
+// }
