fcb09bec5ee75504310144c4923c739721e9c5bc
SimonWillerTPS <simonadrien.willer@gmail.com>
2021-04-17 14:16:17 +0200

    continuing documentation

--- include/game.hpp
+++ include/game.hpp
@@ -3,7 +3,7 @@
 
 /**
  * @file    game.hpp
- * @brief   Defines the Game class
+ * @brief   Defines the Game class.
  * @authors Luca Scimone & Simon Willer
  */
 
@@ -20,16 +20,16 @@
 #include "menu.hpp"
 #include "session.hpp"
 
-#define FONT_PATH     "res/font.ttf"
-#define DROPPED_PATH  "res/dropped.mp3"
-#define GAMEOVER_PATH "res/gameover.mp3"
-#define LINE_PATH     "res/line.mp3"
-#define PAUSE_PATH    "res/pause.wav"
-#define MUSIC_PATH    "res/tetris_music.mp3"
-#define SELECT_PATH   "res/select.mp3"
-#define ROTATE_PATH   "res/rotate.mp3"
+#define FONT_PATH     "res/font.ttf"         // @def Path to the font file.
+#define DROPPED_PATH  "res/dropped.mp3"      // @def Path to the dropped_sound file.
+#define GAMEOVER_PATH "res/gameover.mp3"     // @def Path to the gameover_sound file.
+#define LINE_PATH     "res/line.mp3"         // @def Path to the line_sound file.
+#define PAUSE_PATH    "res/pause.wav"        // @def Path to the pause_sound file.
+#define MUSIC_PATH    "res/tetris_music.mp3" // @def Path to the music file.
+#define SELECT_PATH   "res/select.mp3"       // @def Path to the select_sound file.
+#define ROTATE_PATH   "res/rotate.mp3"       // @def Path to the rotate_sound file.
 
-#define DEFAULT_SOUND   10
+#define DEFAULT_SOUND 10 // @def Default volume level. (Max is 20.)
 
 /**
  * @class   Game
@@ -42,31 +42,31 @@ class Game
         // pthread_t thread_player ;
         // pthread_t thread_IA ;
 
-        SDL_Window*   window         ; /**< Window used to run the game. */
-        SDL_Renderer* renderer       ; /**< Renderer used to render the screen. */             
-        TTF_Font*     font           ; /**< Font used to write texts. */
-        Mix_Music*    music          ; /**< Music that is played during the game. */
-        Mix_Chunk*    sound_dropped  ; /**< Sound played when a piece is placed. */
-        Mix_Chunk*    sound_gameover ; /**< Sound played when the player loses the game. */
-        Mix_Chunk*    sound_line     ; /**< Sound played when lines are completed. */
-        Mix_Chunk*    sound_select   ; /**< Sound played in the menus, during selections. */
-        Mix_Chunk*    sound_rotate   ; /**< Sound played when the piece rotates. */
-        Mix_Chunk*    sound_pause    ; /**< Sound played when the game is paused. */
+        SDL_Window*   window         ; //< Window used to run the game.
+        SDL_Renderer* renderer       ; //< Renderer used to render the screen.  
+        TTF_Font*     font           ; //< Font used to write texts.
+        Mix_Music*    music          ; //< Music that is played during the game.
+        Mix_Chunk*    sound_dropped  ; //< Sound played when a piece is placed.
+        Mix_Chunk*    sound_gameover ; //< Sound played when the player loses the game.
+        Mix_Chunk*    sound_line     ; //< Sound played when lines are completed.
+        Mix_Chunk*    sound_select   ; //< Sound played in the menus, during selections.
+        Mix_Chunk*    sound_rotate   ; //< Sound played when the piece rotates.
+        Mix_Chunk*    sound_pause    ; //< Sound played when the game is paused.
 
-        Menu* menu     ; /**< Menu used for the "main menu" of the game. */ 
-        Menu* settings ; /**< Menu used for the "settings menu" of the game.*/
+        Menu* menu     ; //< Menu used for the "main menu" of the game.
+        Menu* settings ; //< Menu used for the "settings menu" of the game.
 
-        Session* session_player ; /**< Session launched for the player to play. */
-        Session* session_IA     ; /**< Session launched for the IA to play. */
+        Session* session_player ; //< Session launched for the player to play.
+        Session* session_IA     ; //< Session launched for the IA to play.
 
-        std::string win_title      ; /**< String used for the title of the game's window.*/
-        int         win_width      ; /**< Int used to set the window's width. */
-        int         win_height     ; /**< Int used to set the window's height. */
-        int         tile_size = 30 ; /**< Int used to set the size of the game's tiles. */
-        bool        running        ; /**< Bool used to know when to stop the game. */
-        int*        starting_level ; /**< Int* used to defines at which level the game starts. */
-        int*        volume_music   ; /**< Int* used to set the music's volume. */
-        int*        volume_chunk   ; /**< Int* used to set the sound effect's volume. */
+        std::string win_title      ; //< String used for the title of the game's window
+        int         win_width      ; //< Int used to set the window's width.
+        int         win_height     ; //< Int used to set the window's height.
+        int         tile_size = 30 ; //< Int used to set the size of the game's tiles.
+        bool        running        ; //< Bool used to know when to stop the game.
+        int*        starting_level ; //< Int* used to defines at which level the game starts.
+        int*        volume_music   ; //< Int* used to set the music's volume.
+        int*        volume_chunk   ; //< Int* used to set the sound effect's volume.
 
     public :
 
--- include/session.hpp
+++ include/session.hpp
@@ -1,6 +1,12 @@
 #ifndef SESSION_HPP
 #define SESSION_HPP
 
+/**
+ * @file    session.hpp
+ * @brief   Defines the Session class.
+ * @authors Luca Scimone & Simon Willer
+ */
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
@@ -12,56 +18,56 @@
 #include "key.hpp"
 #include "timer.hpp"
 
-#define FPS 60
-#define TICK_PER_FRAME 1000/FPS
+#define FPS 60                  //@def The number of maximum FPS.
+#define TICK_PER_FRAME 1000/FPS //@def Used to calculate framerate. 
 
 class Board ;
 
+/**
+ * @class   Session
+ * @brief 	This class is used to launch, render and manage the gameplay.
+ */
 class Session
 {
     private :
 
-        // Timers :
-        LTimer* fps_timer ;
-        LTimer* cap_timer ;
+        LTimer* fps_timer ; //< Timer used to calculate FPS. 
+        LTimer* cap_timer ; //< Timer used to set a maximum framerate.
 
-        // Attributes for renderer coord. and sizes :
-        int win_orig_x ;
-        int win_orig_y ;
-        int width ;
-        int height ;
-        int tile_size ;
-
-        // SDL :
-            // Given by Game :
-        SDL_Renderer* renderer ;
-        SDL_Event*    event ;
-        TTF_Font*     font ;
-        Mix_Music*    music ;
-        Mix_Chunk*    sound_dropped ;
-        Mix_Chunk*    sound_gameover ;
-        Mix_Chunk*    sound_line ;
-        Mix_Chunk*    sound_rotate ;
-        Mix_Chunk*    sound_pause ;
+        int win_orig_x ; //< Int used so set the original x coordinate to render the game.
+        int win_orig_y ; //< Int used so set the original y coordinate to render the game.
+        int width      ; //< Int used to set the width of the game.
+        int height     ; //< Int used to set the height of the game.
+        int tile_size  ; //< Int used to set the tile's size of the game.
+    
+        SDL_Renderer* renderer       ;  //< Renderer used to render the screen.
+        SDL_Event*    event          ;  //< Event used to detect keyboard inputs.
+        TTF_Font*     font           ;  //< Font used to write text.
+        Mix_Music*    music          ;  //< Music played during the game.
+        Mix_Chunk*    sound_dropped  ;  //< Sound played when a piece is placed.
+        Mix_Chunk*    sound_gameover ;  //< Sound played when the player loses the game.
+        Mix_Chunk*    sound_line     ;  //< Sound played when lines are completed.
+        Mix_Chunk*    sound_rotate   ;  //< Sound played when the piece rotates.
+        Mix_Chunk*    sound_pause    ;  //< Sound played when the game is paused.
             // Local :
-        SDL_Surface* score_surface ;
-        SDL_Surface* level_surface ;
-        SDL_Surface* lines_surface ;    
-        SDL_Texture* score_texture ;
-        SDL_Texture* level_texture ;
-        SDL_Texture* lines_texture ;
-        SDL_Rect rectangle ;
-        SDL_Rect level_rect ;
-        SDL_Rect lines_rect ;
-        SDL_Rect score_rect ;
+        SDL_Surface* score_surface ; //< Surface used to render. 
+        SDL_Surface* level_surface ; //< Surface used to render.
+        SDL_Surface* lines_surface ; //< Surface used to render.   
+        SDL_Texture* score_texture ; //< Texture used to render.
+        SDL_Texture* level_texture ; //< Texture used to render.
+        SDL_Texture* lines_texture ; //< Texture used to render.
+        SDL_Rect rectangle         ; //< Rectangle used to render.
+        SDL_Rect level_rect        ; //< Rectangle used to render.
+        SDL_Rect lines_rect        ; //< Rectangle used to render.
+        SDL_Rect score_rect        ; //< Rectangle used to render.
         
         // Board :
-        Board* board ;
+        Board* board ; //< Board that manages the game's calculations.
 
         // FPS management :
-        float avgFPS ;
-        int   counted_frames = 0 ;
-        int   frame_Ticks ;
+        float avgFPS             ; //< 
+        int   counted_frames = 0 ; //<
+        int   frame_Ticks        ; //<
 
         // Strings to render at screen :
         std::string     string_score ;
